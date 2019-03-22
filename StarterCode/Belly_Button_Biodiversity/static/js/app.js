85e6b6194838fe505f760c303bac2b4a2f431999
Chrisben191 <christophben191@gmail.com>
2019-03-22 14:24:05 -0600

    updated to include bubble chart

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,18 +1,16 @@
 function buildMetadata(sample) {
   d3.json(`metadata/${sample}`).then(function (data) {
-    console.log(data);
+    // console.log(data);
     let myHtmlBlock = d3.select('#sample-metadata').html(" ");
-    // var wFreq = data.wfreq;
-    // console.log(wFreq);
     Object.keys(data).forEach(key => {
       myHtmlBlock.append('p').text(key + ": " + data[key]);
-      console.log(key);
+      // console.log(key);
     });
   })
 }
-
+////////////////////////////////////////////////////////////////////////////////
 function buildCharts(sample) {
-  console.log(sample);
+  // console.log(sample);
   let url = `samples/${sample}`; // this is really the ID of the person
   d3.json(url).then(function (data) {
 
@@ -49,12 +47,80 @@ function buildCharts(sample) {
     var data = [trace1];
     Plotly.newPlot('bubble', data);
   })
-}
+    // // Plotly Guage
+    // let metadata = `metadata/${sample}`;
+    // let wfreq = d3.json(metadata);
+    // let wFreq = (wfreq["WFREQ"]);
+    // console.log(wFreq);
+    // var level = parseInt(wFreq) * 20;
 
+    // // Trig to calc meter point
+    // var degrees = 180 - level,
+    //     radius = .5;
+    // var radians = degrees * Math.PI / 180;
+    // var x = radius * Math.cos(radians);
+    // var y = radius * Math.sin(radians);
+
+    // // Path: may have to change to create a better triangle
+    // var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+    //     pathX = String(x),
+    //     space = ' ',
+    //     pathY = String(y),
+    //     pathEnd = ' Z';
+    // var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+    // var data = [{ type: 'scatter',
+    //   x: [0], y:[0],
+    //     marker: {size: 28, color:'850000'},
+    //     showlegend: false,
+    //     name: 'Wash Frequency',
+    //     text: level,
+    //     hoverinfo: 'text+name'},
+    //   { values: [50/9,50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9,50/9, 50],
+    //   rotation: 90,
+    //   text: ['9-8', '8-7', '7-6', '6-5', '5-4', '4-3', '3-2', '2-1', '1-0', ''],
+    //   textinfo: 'text',
+    //   textposition:'inside',
+    //   marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+    //                         'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+    //                         'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+    //                         'rgba(255, 255, 255, 0)']},
+    //   labels: ['9-8', '8-7', '7-6', '6-5', '5-4', '4-3', '3-2', '2-1', '1-0', ''],
+    //   hoverinfo: 'label',
+    //   hole: .5,
+    //   type: 'pie',
+    //   showlegend: false
+    // }];
+
+    // var layout = {
+    //   shapes:[{
+    //       type: 'path',
+    //       path: path,
+    //       fillcolor: '850000',
+    //       line: {
+    //         color: '850000'
+    //       }
+    //     }],
+    //   title: 'Gauge',
+    //   height: 1000,
+    //   width: 1000,
+    //   xaxis: {zeroline:false, showticklabels:false,
+    //             showgrid: false, range: [-1, 1]},
+    //   yaxis: {zeroline:false, showticklabels:false,
+    //             showgrid: false, range: [-1, 1]}
+    // };
+
+    // Plotly.newPlot('gauge', data, layout);
+
+
+
+
+
+}
+////////////////////////////////////////////////////////////////////////////////
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
@@ -63,22 +129,19 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
 }
-
+//////////////////////////////////////////////////////////////////////////////// 
 function optionChanged(newSample) {
   // receives the Id of the pesron who gave the sample
-
-
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
 }
-
+////////////////////////////////////////////////////////////////////////////////
 // Initialize the dashboard
 init();
